d3e3c2f1a063d3bfb75feb569c7760d71f3c2e48
nasreaper <frank.spencer.3@us.af.mil>
2024-10-15 06:32:51 +0000

    adding repit to git hub

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("I am signging up for Replit's 100 days of Python challenge!\nI will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.\nI'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be.\nNo excuses for not coding from the middle of a field!\n\n\nI am feeling like a baws!\n\n\nYou can follow my progress at replit.com/@frankspencer3")
+print(
+    "I am signging up for Replit's 100 days of Python challenge!\nI will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.\nI'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be.\nNo excuses for not coding from the middle of a field!\n\n\nI am feeling like a baws!\n\n\nYou can follow my progress at replit.com/@frankspencer3"
+)
 
+print(
+    """I am signing up for Replit's 100 days of Python challenge!\nI will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.\nI'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be.\nNo excuses for not coding from the middle of a field!\n\n\nI am feeling like a baws!\n\n\nYou can follow my progress at replit.com/@frankspencer3 """
+)
 
-print("""I am signing up for Replit's 100 days of Python challenge!\nI will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.\nI'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be.\nNo excuses for not coding from the middle of a field!\n\n\nI am feeling like a baws!\n\n\nYou can follow my progress at replit.com/@frankspencer3 """)
+#change to figure out git
+#2nd change for git
